a7243162326f5a72073bab1d171ae23e22466718
GOHU <GOHU@GOHU-T490.oradev.oraclecorp.com>
2020-04-13 15:21:36 -0400

    first commit

---
+++ dglzdealer.js
@@ -0,0 +1,13 @@
+function go(){
+    var array = new Array();
+    array[0] = "Player 1";
+    array[1] = "Player 2";
+    array[2] = "Player 3";
+    array[3] = "Player 4";
+    array[4] = "Player 5";
+    array[5] = "Player 6";
+    for (var i=0; i < array.length; i++){
+        document.write("<li>" + array[i] + "<br />");
+    }
+	
+}
